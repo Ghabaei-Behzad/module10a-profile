42421086404ca76718a0bdb65ae7211932bb9828
Ghabaei-Behzad <Ghabaei_Behzad@student.smc.edu>
2026-02-07 14:05:00 -0800

    Update App.jsx
    improvements and editing

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ function App() {
         name="J.K. Rowling"
         occupation="Author"
         funFact="She originally published the first Harry Potter book under the name Joanne Rowling."
+        details=" is an incredibly influential author whose works have shaped modern literature. They are known for their compelling narratives and intricate character development."
       />
       
       <Profile
         name="Serena Williams"
-        occupation="Athlete (Tennis Player)"
+        occupation="Tennis Player"
         funFact="She has won 23 Grand Slam singles titles, more than any other woman in the Open Era."
+        details=" won four Olympic gold medals, three in women's doubles an all-time joint record in tennis, shared with her sister.  The duo are the only women in the Open Era to win Olympic gold in both singles and doubles."
       />
 
     </div>
